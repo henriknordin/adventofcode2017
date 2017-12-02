39a61bacb78bee674440a2bd7d37a78640a735f3
Henrik Nordin <nordhenr@gmail.com>
2017-12-02 16:38:35 +0100

    Solving puzzles for day 1

---
+++ src/Lib.hs
@@ -0,0 +1,26 @@
+module Lib
+    ( captcha
+    , captcha2
+    ) where
+
+import Data.Char (digitToInt)
+
+captcha :: String -> Int
+captcha xs = calculate (digitToInt $ head xs) (map digitToInt xs)
+  where
+    calculate :: Int -> [Int] -> Int
+    calculate a [x] = if a == x then x else 0
+    calculate a (x:y:xs) = s + calculate a (y:xs) 
+                           where s = if x == y then x else 0
+
+
+captcha2 :: String -> Int
+captcha2 xs = calculate $ splitHalf $ map digitToInt xs
+  where
+    calculate :: ([Int], [Int]) -> Int
+    calculate (xs, ys) = sum $ map (\(a, _) -> 2 * a) $ filter (\(a, b) -> a == b) $ zip xs ys
+                     
+splitHalf :: [a] -> ([a], [a])
+splitHalf xs = splitAt half xs
+               where half = length xs `div` 2 
+
