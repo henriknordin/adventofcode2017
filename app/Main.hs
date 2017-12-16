bb1bfe55fafc8295e0080f5b35b206dd146f1496
Henrik Nordin <nordhenr@gmail.com>
2017-12-16 20:12:58 +0100

    Solves puzzles for day 16

--- app/Main.hs
+++ app/Main.hs
@@ -17,73 +17,79 @@ import qualified Advent12 as A12 (answer1, answer2)
 import qualified Advent13 as A13 (answer1, answer2)
 import qualified Advent14 as A14 (answer1, answer2)
 import qualified Advent15 as A15 (answer1, answer2)
+import qualified Advent16 as A16 (answer1, answer2)
+--, answer2)
 
 main :: IO ()
 main = do 
-  content1 <- readFile "data/advent1.txt"
-  let ls1 = lines content1
-  putStrLn $ "Advent 1-1: " ++ show (A1.answer1 ls1) 
-  putStrLn $ "Advent 1-2: " ++ show (A1.answer2 ls1)
-  
-  content2 <- readFile "data/advent2.txt"
-  let ls2 = lines content2
-  putStrLn $ "Advent 2-1: " ++ show (A2.answer1 ls2) 
-  putStrLn $ "Advent 2-2: " ++ show (A2.answer2 ls2)
+--  content1 <- readFile "data/advent1.txt"
+--  let ls1 = lines content1
+--  putStrLn $ "Advent 1-1: " ++ show (A1.answer1 ls1) 
+--  putStrLn $ "Advent 1-2: " ++ show (A1.answer2 ls1)
+--  
+--  content2 <- readFile "data/advent2.txt"
+--  let ls2 = lines content2
+--  putStrLn $ "Advent 2-1: " ++ show (A2.answer1 ls2) 
+--  putStrLn $ "Advent 2-2: " ++ show (A2.answer2 ls2)
+--
+--  putStrLn $ "Advent 3-1: " ++ show (A3.answer1 361527)
+--  putStrLn $ "Advent 3-2: " ++ show (A3.answer2 361527)
+--  
+--  content4 <- readFile "data/advent4.txt"
+--  let ls4 = lines content4
+--  putStrLn $ "Advent 4-1: " ++ show (A4.answer1 ls4) 
+--  putStrLn $ "Advent 4-2: " ++ show (A4.answer2 ls4)
+-- 
+--  content5 <- readFile "data/advent5.txt"
+--  let ls5 = lines content5
+--  putStrLn $ "Advent 5-1: " ++ show (A5.answer1 ls5)
+--  -- slow! 
+--  -- putStrLn $ "Advent 5-2: " ++ show (A5.answer2 ls5)
+--  
+--  content6 <- readFile "data/advent6.txt"
+--  let ls6 = lines content6
+--  -- slow!
+--  -- putStrLn $ "Advent 6-1: " ++ show (A6.answer1 ls6)
+--  -- putStrLn $ "Advent 6-2: " ++ show (A6.answer2 ls6)
+--  
+--  content7 <- readFile "data/advent7.txt"
+--  let ls7 = lines content7
+--  let tower = buildTree ls7
+--
+--  putStrLn $ "Advent 7-1: " ++ show (root tower)
+--  putStrLn $ "Advent 7-2: " ++ show (unbalanced tower 0)
+--
+--  content8 <- readFile "data/advent8.txt"
+--  let ls8 = lines content8
+--  --putStrLn $ "Advent 8-1: " ++ show (advent8 $ fst $ process ls8)
+--  --putStrLn $ "Advent 8-2: " ++ show (snd $ process ls8)
+--  
+--  content9 <- readFile "data/advent9.txt"
+--  let ls9 = lines content9
+--  putStrLn $ "Advent 9-1: " ++ show (scoreStream $ head ls9 )
+--  putStrLn $ "Advent 9-2: " ++ show (countGarbage $ head ls9)
+--
+--  putStrLn $ "Advent 10-1: " ++ show answer10_1
+--  putStrLn $ "Advent 10-2: " ++ show answer10_2
+--
+--  content12 <- readFile "data/advent12.txt"
+--  let ls12 = lines content12
+--  putStrLn $ "Advent 12-1: " ++ show (A12.answer1 ls12) 
+--  putStrLn $ "Advent 12-2: " ++ show (A12.answer2 ls12)
+--
+--  content13 <- readFile "data/advent13.txt"
+--  let ls13 = lines content13
+--  putStrLn $ "Advent 13-1: " ++ show (A13.answer1 ls13) 
+--  -- slow!
+--  -- putStrLn $ "Advent 13-2: " ++ show (A13.answer2 ls13)
+--
+--  --putStrLn $ "Advent 14-1: " ++ show (A14.answer1 "ugkiagan")
+--  --putStrLn $ "Advent 14-2: " ++ show (A14.answer2 "ugkiagan")
 
-  putStrLn $ "Advent 3-1: " ++ show (A3.answer1 361527)
-  putStrLn $ "Advent 3-2: " ++ show (A3.answer2 361527)
-  
-  content4 <- readFile "data/advent4.txt"
-  let ls4 = lines content4
-  putStrLn $ "Advent 4-1: " ++ show (A4.answer1 ls4) 
-  putStrLn $ "Advent 4-2: " ++ show (A4.answer2 ls4)
- 
-  content5 <- readFile "data/advent5.txt"
-  let ls5 = lines content5
-  putStrLn $ "Advent 5-1: " ++ show (A5.answer1 ls5)
-  -- slow! 
-  -- putStrLn $ "Advent 5-2: " ++ show (A5.answer2 ls5)
-  
-  content6 <- readFile "data/advent6.txt"
-  let ls6 = lines content6
-  -- slow!
-  -- putStrLn $ "Advent 6-1: " ++ show (A6.answer1 ls6)
-  -- putStrLn $ "Advent 6-2: " ++ show (A6.answer2 ls6)
-  
-  content7 <- readFile "data/advent7.txt"
-  let ls7 = lines content7
-  let tower = buildTree ls7
+--  putStrLn $ "Advent 15-1: " ++ show (A15.answer1 883 879)
+--  putStrLn $ "Advent 15-2: " ++ show (A15.answer2 883 879)
 
-  putStrLn $ "Advent 7-1: " ++ show (root tower)
-  putStrLn $ "Advent 7-2: " ++ show (unbalanced tower 0)
-
-  content8 <- readFile "data/advent8.txt"
-  let ls8 = lines content8
-  --putStrLn $ "Advent 8-1: " ++ show (advent8 $ fst $ process ls8)
-  --putStrLn $ "Advent 8-2: " ++ show (snd $ process ls8)
-  
-  content9 <- readFile "data/advent9.txt"
-  let ls9 = lines content9
-  putStrLn $ "Advent 9-1: " ++ show (scoreStream $ head ls9 )
-  putStrLn $ "Advent 9-2: " ++ show (countGarbage $ head ls9)
-
-  putStrLn $ "Advent 10-1: " ++ show answer10_1
-  putStrLn $ "Advent 10-2: " ++ show answer10_2
-
-  content12 <- readFile "data/advent12.txt"
-  let ls12 = lines content12
-  putStrLn $ "Advent 12-1: " ++ show (A12.answer1 ls12) 
-  putStrLn $ "Advent 12-2: " ++ show (A12.answer2 ls12)
-
-  content13 <- readFile "data/advent13.txt"
-  let ls13 = lines content13
-  putStrLn $ "Advent 13-1: " ++ show (A13.answer1 ls13) 
-  -- slow!
-  -- putStrLn $ "Advent 13-2: " ++ show (A13.answer2 ls13)
-
-  --putStrLn $ "Advent 14-1: " ++ show (A14.answer1 "ugkiagan")
-  --putStrLn $ "Advent 14-2: " ++ show (A14.answer2 "ugkiagan")
-
-  putStrLn $ "Advent 15-1: " ++ show (A15.answer1 883 879)
-  putStrLn $ "Advent 15-2: " ++ show (A15.answer2 883 879)
-
+  content16 <- readFile "data/advent16.txt"
+  let ls16 = lines content16
+  putStrLn $ "Advent 16-1: " ++ show (A16.answer1 $ head ls16) 
+  putStrLn $ "Advent 16-2: " ++ show (A16.answer2 $ head ls16)
---
+++ src/Advent16.hs
@@ -0,0 +1,69 @@
+module Advent16 where
+
+import Data.List.Split (splitOn)
+import qualified Data.Foldable as F (toList)
+import qualified Data.Sequence as S
+import Data.Maybe (fromJust)
+
+
+programs :: String
+programs = "abcdefghijklmnop"
+
+answer1 :: String -> String
+answer1 inp = let steps = splitOn "," inp
+              in dance "abcdefghijklmnop" steps
+
+-- pnhajoekigcbflmd
+answer2 :: String -> String
+answer2 inp = let instructions = splitOn "," inp
+                  infIns = repeat instructions
+                  infStates = danceSequence programs infIns
+                  cycleLength = snd $ head $ dropWhile (\(state,i) -> state /= programs || i == 0) $ zip infStates [0,1..]
+                  cycles = 1000000000 `mod` cycleLength
+              in fst $ head $ dropWhile (\(_, i) -> i /= cycles) $ zip infStates [0,1..]
+
+danceSequence :: String -> [[String]] -> [String]
+danceSequence ps (s:steps) = ps : danceSequence (dance ps s) (tail steps)
+
+
+dance :: String -> [String] -> String
+dance = foldl dancestep
+  where
+    dancestep :: String -> String -> String
+    dancestep ps ('s':xs) = spin ps (toInt xs)
+    dancestep ps ('x':xs) = let (a,b) = parseX xs
+                            in exchange ps a b
+    dancestep ps ('p':xs) = let (a, b) = parseP xs
+                            in partner ps a b
+parseX :: String -> (Int, Int)
+parseX s = let a = takeWhile (/= '/') s
+               b = drop 1 (dropWhile (/= '/') s)
+           in (toInt a, toInt b)
+
+parseP :: String -> (Char, Char)
+parseP s = let a = head s
+               b = head $ drop 1 (dropWhile (/= '/') s)
+           in (a, b)
+
+toInt :: String -> Int
+toInt s = read s :: Int
+
+spin :: [a] -> Int -> [a] 
+spin xs s = let (beg, end) = splitAt (length xs - s) xs
+            in end ++ beg
+
+exchange :: [a] -> Int -> Int -> [a]
+exchange xs a b = let seq = S.fromList xs
+                      va  = S.index seq a
+                      vb  = S.index seq b
+                      seq' = S.update a vb (S.update b va seq)
+                  in F.toList seq' 
+
+partner :: Eq a => [a] -> a -> a -> [a]
+partner xs a b = let seq = S.fromList xs
+                     ia  = fromJust $ S.elemIndexL a seq 
+                     ib  = fromJust $ S.elemIndexL b seq
+                     seq' = S.update ib a (S.update ia b seq)
+                  in F.toList seq' 
+
+--s1, a spin of size 1: eabcd
