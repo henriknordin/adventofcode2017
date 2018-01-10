aa21ffd286b9b5496cdd78856ced71778ff2a5fc
Henrik Nordin <nordhenr@gmail.com>
2018-01-10 21:42:47 +0100

    Fixed solution for day 17 part 2

--- src/Advent17.hs
+++ src/Advent17.hs
@@ -1,69 +1,69 @@
---{-# LANGUAGE DuplicateRecordFields #-} 
-module Advent17 
-    ( parseInput
-    , answer1
-    , answer2
-    ) where
+module Advent17
+  ( parseInput
+  , answer1
+  , answer2
+  ) where
 
-import qualified Data.Sequence as S (Seq, fromList, singleton, insertAt, index, dropWhileL)
-
-data Buffer = MkBuffer { buffer :: S.Seq Int
-                       , index  :: !Int
-                       } deriving Show
+import qualified Data.Sequence as S (Seq, dropWhileL, fromList, index, insertAt, singleton)
+import Debug.Trace
 
 parseInput :: String -> Int
 parseInput = (\x -> read x :: Int) . head . lines
 
-mkBuffer :: Buffer
-mkBuffer = MkBuffer (S.singleton 0) 0
+data Buffer = MkBuffer
+  { buffer :: S.Seq Int
+  , index  :: !Int
+  } deriving (Show)
+
+data NoBuffer = MkNoBuffer
+  { position  :: !Int
+  , size      :: !Int
+  , zeroAt    :: !Int
+  , afterZero :: !Int
+  } deriving (Show)
 
 answer1 :: Int -> Int
 answer1 s = S.index (S.dropWhileL (/= 2017) $ buffer $ process s) 1
 
 answer2 :: Int -> Int
-answer2 s = process2 mkNoBuffer 0 s 1
-
---process :: Int -> Buffer
---process size = foldl (\b a -> step b size a) (mkBuffer 2017) [1,2..2017]
+answer2 = process2 buf
+  where
+    buf = MkNoBuffer 0 1 0 0
 
 process :: Int -> Buffer
-process size = foldr (\a b -> step b size a) mkBuffer (reverse [1,2..2017])
-
-
-process2 :: NoBuffer -> Int -> Int -> Int -> Int
-process2 tz ind sz 50000000 = nextValue $ snd $ step' tz ind sz 50000000
-process2 tz ind sz        n = let (ind', tz') = step' tz ind sz n
-                              in  process2 tz' ind' sz (n+1)
-
-process2' :: NoBuffer -> Int -> Int -> Int -> Int
-process2' tz ind sz n
-  | n <= 50000000 = let (ind', tz') = step' tz ind sz n
-                              in  process2 tz' ind' sz (n+1)
-  | otherwise     = nextValue tz
-
---process2' tz ind sz 50000000 = nextValue $ snd $ step' tz ind sz 50000000
---process2' tz ind sz        n = 
+process size = foldr (\a b -> step b size a) buf (reverse [1,2 .. 2017])
+  where 
+    buf = MkBuffer (S.singleton 0) 0
 
 step :: Buffer -> Int -> Int -> Buffer
-step b n v = let ind = (index b + n) `mod` v
-                 b' = S.insertAt (ind + 1) v (buffer b)
-           in MkBuffer b' (ind + 1) 
+step b n v =
+  let ind = (index b + n) `mod` v
+      b' = S.insertAt (ind + 1) v (buffer b)
+  in MkBuffer b' (ind + 1)
 
-data NoBuffer = MkNoBuffer   { index' :: !Int
-                             , zeroIndex :: !Int
-                             , nextValue :: !Int
-                             } deriving Show
+process2 :: NoBuffer -- ^ The current state of the ring buffer 
+         -> Int      -- ^ The step size
+         -> Int      -- ^ The value after zero
+process2 buf step
+  | size buf <= 50000000 =
+      let buf' = step' buf step
+      in process2 buf' step
+  | otherwise = afterZero buf
 
-mkNoBuffer = MkNoBuffer 0 0 0
+step' :: NoBuffer  -- ^ The current state of the ring buffer 
+      -> Int       -- ^ The step size
+      -> NoBuffer  -- ^ The updated state of the buffer
+step' buf step =
+  let nextPosition = (position buf + step) `mod` size buf
+  in updateTracker buf nextPosition
 
-step' :: NoBuffer -> Int -> Int -> Int -> (Int, NoBuffer) 
-step' tz ind sz v = let ind' = (ind + sz) `mod` v
-                        tz' = evalTracker tz ind' v
-                    in (ind' + 1, tz')
-                
-evalTracker :: NoBuffer -> Int -> Int -> NoBuffer
-evalTracker tz ind v
-  | ind < index' tz = MkNoBuffer 0 (zeroIndex tz + 1) (nextValue tz)
-  | ind == index' tz = MkNoBuffer 0 (zeroIndex tz) v
-  | otherwise       = tz
+updateTracker :: NoBuffer -- ^ The current buffer
+              -> Int      -- ^ The next position to update
+              -> NoBuffer -- ^ The updated buffer
+updateTracker (MkNoBuffer _ size' zeroAt' afterZero') nextPosition
+  | nextPosition < zeroAt'  = partialBuf (zeroAt' + 1) afterZero'
+  | nextPosition == zeroAt' = partialBuf zeroAt' size'
+  | otherwise               = partialBuf zeroAt' afterZero'
+  where 
+    partialBuf = MkNoBuffer (nextPosition + 1) (size' + 1)
 
